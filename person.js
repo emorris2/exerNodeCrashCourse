6d1ca2975cdfc61840e736393d3e93ed4f4654a7
emorris2 <emaemorris@yahoo.com>
2020-12-01 15:02:13 -0700

    Assignment Due 12-1

---
+++ person.js
@@ -0,0 +1,24 @@
+// Module Wrapper Function
+//(function (exports, require, modue, __filename, __dirname){
+
+//})
+
+//const person = {
+    //name: 'Emma Morris',
+    //age:36
+//}
+
+//console.log(__dirname, __filename);
+
+class Person {
+    constructor(name, age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    greeting() {
+        console.log(`My name is ${this.name} and I am ${this.age}`)
+    }
+}
+module.exports = Person;
+
